e413a085fd97cec96e2b2e19eff2bc196f124ff5
Koussai9 <88429637+Koussai9@users.noreply.github.com>
2024-05-20 10:50:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import { getDatabase, ref } from "@firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiBYgnFxAXKBRIgxO-rUEJyMDTFbH5PHs",
-  authDomain: "wheel-f5238.firebaseapp.com",
-  databaseURL: "https://wheel-f5238-default-rtdb.firebaseio.com",
-  projectId: "wheel-f5238",
-  storageBucket: "wheel-f5238.appspot.com",
-  messagingSenderId: "851756482678",
-  appId: "1:851756482678:web:28cfcdbeb18eeab3b1df57",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 if (firebase.apps.length === 0) {
